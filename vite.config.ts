8478a769e942b1dc75e9672c58e02b926a21fcce
dangtruongthanhhoang-gif <dangtruongthanhhoang@gmail.com>
2026-06-12 13:20:21 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Vui cung WC2026/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
